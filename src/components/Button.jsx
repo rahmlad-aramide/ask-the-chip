eebdbebddaa55c1e575085f4bdc637f964cf6bb2
ABDRAHMAN OLADIMEJI <rahmlad@localhost.localdomain>
2023-02-20 15:29:22 +0100

    update: added the required assets file for landing page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Button = ({path, title}) => {
   return (
     <Link to={`/${path}`}>
-        <button className='bg-primary text-white py-2 px-4 rounded'>
+        <button className='bg-primary text-white py-2 px-4 rounded hover:scale-110 transition duration-200'>
             {title}
         </button>
     </Link>
